4f68026554bea59b8f59689e0381e52038daf6b4
Davix316 <david.curipoma@epn.edu.ec>
2020-09-27 19:13:34 -0500

    #65 Conexion

--- cursos/routes/api.php
+++ cursos/routes/api.php
@@ -13,83 +13,86 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['middleware' => ['cors']], function () {
+    Route::post('register', 'UserController@register');
+    Route::post('login', 'UserController@authenticate');
+    Route::get('courses', 'CourseController@index');
+    Route::get('courses/{course}', 'CourseController@show');
 
-Route::post('register', 'UserController@register');
-Route::post('login', 'UserController@authenticate');
-Route::get('courses', 'CourseController@index');
-Route::get('courses/{course}', 'CourseController@show');
 
-Route::group(['middleware' => ['jwt.verify']], function() {
-    Route::get('user', 'UserController@getAuthenticatedUser');
+    Route::group(['middleware' => ['jwt.verify']], function () {
+        Route::get('user', 'UserController@getAuthenticatedUser');
+        Route::post('logout', 'UserController@logout');
 
-    //Registros
-    Route::get('registers', 'RegisterController@index');
-    Route::get('registers/{register}', 'RegisterController@show');
-    Route::post('registers', 'RegisterController@store');
-    Route::put('registers/{register}', 'RegisterController@update');
-    Route::delete('registers/{register}', 'RegisterController@delete');
-    //Preguntas
-    Route::get('questions', 'QuestionController@index');
-    Route::get('questions/{question}', 'QuestionController@show');
+        //Registros
+        Route::get('registers', 'RegisterController@index');
+        Route::get('registers/{register}', 'RegisterController@show');
+        Route::post('registers', 'RegisterController@store');
+        Route::put('registers/{register}', 'RegisterController@update');
+        Route::delete('registers/{register}', 'RegisterController@delete');
+        //Preguntas
+        Route::get('questions', 'QuestionController@index');
+        Route::get('questions/{question}', 'QuestionController@show');
 
-    Route::get('levels/{level}/questions', 'QuestionController@ind');
-    Route::post('questions', 'QuestionController@store');
-    Route::put('questions/{question}', 'QuestionController@update');
-    Route::delete('questions/{question}', 'QuestionController@delete');
-    //Respuestas
-    Route::get('answers', 'AnswerController@index');
-    Route::get('answers/{answer}', 'AnswerController@show');
+        Route::get('levels/{level}/questions', 'QuestionController@ind');
+        Route::post('questions', 'QuestionController@store');
+        Route::put('questions/{question}', 'QuestionController@update');
+        Route::delete('questions/{question}', 'QuestionController@delete');
+        //Respuestas
+        Route::get('answers', 'AnswerController@index');
+        Route::get('answers/{answer}', 'AnswerController@show');
 
-    Route::get('questions/{question}/answers', 'AnswerController@ind');
+        Route::get('questions/{question}/answers', 'AnswerController@ind');
 
-    Route::post('answers', 'AnswerController@store');
-    Route::put('answers/{answer}', 'AnswerController@update');
-    Route::delete('answers/{answer}', 'AnswerController@delete');
-    //Respuestas seleccionadas
-    Route::get('select_answers', 'SelectAnswerController@index');
-    Route::get('select_answers/{selectAnswer}', 'SelectAnswerController@show');
-    Route::post('select_answers', 'SelectAnswerController@store');
-    Route::put('select_answers/{selectAnswer}', 'SelectAnswerController@update');
-    Route::delete('select_answers/{selectAnswer}', 'SelectAnswerController@delete');
-    //Historial
-    Route::get('records', 'RecordController@index');
-    Route::get('records/{record}', 'RecordController@show');
-    Route::post('records', 'RecordController@store');
-    Route::put('records/{record}', 'RecordController@update');
-    Route::delete('records/{record}', 'RecordController@delete');
-    //Certificados
-    Route::get('registers/{register}/certificates', 'CertificateController@index');
-    Route::get('registers/{register}/certificates/{certificate}', 'CertificateController@show');
-    Route::post('registers/{register}/certificates', 'CertificateController@store');
-    Route::put('registers/{register}/certificates/{certificate}', 'CertificateController@update');
-    Route::delete('registers/{register}/certificates/{certificate}', 'CertificateController@delete');
-    //Cursos
+        Route::post('answers', 'AnswerController@store');
+        Route::put('answers/{answer}', 'AnswerController@update');
+        Route::delete('answers/{answer}', 'AnswerController@delete');
+        //Respuestas seleccionadas
+        Route::get('select_answers', 'SelectAnswerController@index');
+        Route::get('select_answers/{selectAnswer}', 'SelectAnswerController@show');
+        Route::post('select_answers', 'SelectAnswerController@store');
+        Route::put('select_answers/{selectAnswer}', 'SelectAnswerController@update');
+        Route::delete('select_answers/{selectAnswer}', 'SelectAnswerController@delete');
+        //Historial
+        Route::get('records', 'RecordController@index');
+        Route::get('records/{record}', 'RecordController@show');
+        Route::post('records', 'RecordController@store');
+        Route::put('records/{record}', 'RecordController@update');
+        Route::delete('records/{record}', 'RecordController@delete');
+        //Certificados
+        Route::get('registers/{register}/certificates', 'CertificateController@index');
+        Route::get('registers/{register}/certificates/{certificate}', 'CertificateController@show');
+        Route::post('registers/{register}/certificates', 'CertificateController@store');
+        Route::put('registers/{register}/certificates/{certificate}', 'CertificateController@update');
+        Route::delete('registers/{register}/certificates/{certificate}', 'CertificateController@delete');
+        //Cursos
 
-    Route::post('courses', 'CourseController@store');
-    Route::put('courses/{course}', 'CourseController@update');
-    Route::delete('courses/{course}', 'CourseController@delete');
-    //Nivel
-    Route::get('levels', 'LevelController@index');
-    Route::get('levels/{level}', 'LevelController@show');
-    Route::post('levels', 'LevelController@store');
-    Route::put('levels/{level}', 'LevelController@update');
-    Route::delete('levels/{level}', 'LevelController@delete');
-    //Contenido
-    Route::get('contents', 'ContentController@index');
-    Route::get('levels/{level}/contents', 'ContentController@ind');
-    Route::post('contents', 'ContentController@store');
-    Route::put('contents/{content}', 'ContentController@update');
-    Route::delete('contents/{content}', 'ContentController@delete');
+        Route::post('courses', 'CourseController@store');
+        Route::put('courses/{course}', 'CourseController@update');
+        Route::delete('courses/{course}', 'CourseController@delete');
+        //Nivel
+        Route::get('levels', 'LevelController@index');
+        Route::get('levels/{level}', 'LevelController@show');
+        Route::post('levels', 'LevelController@store');
+        Route::put('levels/{level}', 'LevelController@update');
+        Route::delete('levels/{level}', 'LevelController@delete');
+        //Contenido
+        Route::get('contents', 'ContentController@index');
+        Route::get('levels/{level}/contents', 'ContentController@ind');
+        Route::post('contents', 'ContentController@store');
+        Route::put('contents/{content}', 'ContentController@update');
+        Route::delete('contents/{content}', 'ContentController@delete');
 
-    //Pruebas para visualizar usuarios
-    Route::get('users', 'UserController@index');
-    Route::get('users/{user}', 'UserController@show');
+        //Pruebas para visualizar usuarios
+        Route::get('users', 'UserController@index');
+        Route::get('users/{user}', 'UserController@show');
 
-    Route::get('users/{user}/courses', 'CourseController@ind');
+        Route::get('users/{user}/courses', 'CourseController@ind');
 
-    //Route::get('courses/{course}/registers', 'RegisterController@ind');
-    Route::get('courses/{course}/levels', 'LevelController@ind');
+        //Route::get('courses/{course}/registers', 'RegisterController@ind');
+        Route::get('courses/{course}/levels', 'LevelController@ind');
 
+    });
 });
 
 //Usuarios
